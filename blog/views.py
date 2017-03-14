f7410bb7a52fd245745c081056846fc3360cbda8
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-03-14 12:36:38 +1100

    Missing import of post form.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
